378338a1fe26d827586a8b4a3e8e2fdf378f3bd4
park jinyong <jiny3360@nine2021.com>
2023-10-03 15:29:13 +0900

    fin: QR Generator - 기능 추가 - CSS 수정 - 이펙트 수정

---
+++ index.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper');
+generateBtn = wrapper.querySelector('.form button');
+qrInput = wrapper.querySelector('.form input');
+qrImg = wrapper.querySelector('.qr-code img');
+
+generateBtn.addEventListener('click', () => {
+  let qrValue = qrInput.value;
+  if (!qrValue) return;
+  generateBtn.innerText = 'Generating...';
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  qrImg.addEventListener('load', () => {
+    wrapper.classList.add('active');
+    generateBtn.innerText = 'Generate QR Code';
+  });
+});
+
+qrInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    generateBtn.click();
+  }
+
+  if (!qrInput.value) {
+    wrapper.classList.remove('active');
+  }
+});
